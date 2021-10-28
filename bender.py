39928d9ccce1fa6c67963e0fc660f0997c52b8f5
Veit Große <grosse@theva.com>
2021-10-28 22:33:13 +0200

    formated using yapf

--- bender.py
+++ bender.py
@@ -15,7 +15,7 @@ class VGBendingMaterialData(NamedTuple):
     critical_tensil_strain: float
 
 
-class VGBendingSolver():
+class VGBendingSolver:
     _modell: Optional[list[VGBendingMaterialData]] = None
 
     @property
@@ -58,18 +58,21 @@ class VGBendingSolver():
     def parse_material_data(self, data: dict[str, Any]) -> None:
         self._modell = []
         for material in data["Layers"]:
-            parameters = VGBendingMaterialData(name=material["name"],
-                                               is_superconductor=material["isSuperconductor"],
-                                               thickness=material["thickness"],
-                                               E1=material["E1"],
-                                               E2=material["E2"],
-                                               E3=material["E3"],
-                                               sigma1=material["sigma1"],
-                                               sigma2=material["sigma2"],
-                                               critical_tensil_strain=material["criticalTensilStrain"])
+            parameters = VGBendingMaterialData(
+                name=material["name"],
+                is_superconductor=material["isSuperconductor"],
+                thickness=material["thickness"],
+                E1=material["E1"],
+                E2=material["E2"],
+                E3=material["E3"],
+                sigma1=material["sigma1"],
+                sigma2=material["sigma2"],
+                critical_tensil_strain=material["criticalTensilStrain"],
+            )
             self._modell.append(parameters)
 
-    def _min_bending_diameter(self, modell: list[VGBendingMaterialData]) -> Optional[float]:
+    def _min_bending_diameter(
+            self, modell: list[VGBendingMaterialData]) -> Optional[float]:
         class CriticalConditions(NamedTuple):
             pos: float
             material: VGBendingMaterialData
@@ -77,14 +80,14 @@ class VGBendingSolver():
         bending_diameter: Optional[float] = None
 
         superconductors: list[CriticalConditions] = []
-        posValue: float = 0.0
+        pos_value: float = 0.0
 
         for material in modell:
             if not material.is_superconductor:
-                posValue += material.thickness
+                pos_value += material.thickness
             else:
-                superconductor = CriticalConditions(
-                    pos=posValue, material=material)
+                superconductor = CriticalConditions(pos=pos_value,
+                                                    material=material)
                 superconductors.append(superconductor)
 
         for diameter in range(300, 0, -1):
@@ -94,46 +97,52 @@ class VGBendingSolver():
                 print(f"{diameter} mm: Neutral Axis is at y= {neutral_axis}")
 
                 for superconductor in superconductors:
-                    epsilon = self._strain(
-                        superconductor.pos, neutral_axis, float(diameter))
-                    if epsilon > 0 and epsilon > superconductor.material.critical_tensil_strain:
+                    epsilon = self._strain(superconductor.pos, neutral_axis,
+                                           float(diameter))
+                    if (epsilon > 0 and epsilon >
+                            superconductor.material.critical_tensil_strain):
                         print(f"Minimum Bending Diameter is {diameter} mm")
                         bending_diameter = float(diameter)
         return bending_diameter
 
-    def _position_of_neutral_axis(self, diameter: float, modell: list[VGBendingMaterialData]) -> float:
-        maxValue = int(self._total_thickness)
+    def _position_of_neutral_axis(
+            self, diameter: float,
+            modell: list[VGBendingMaterialData]) -> float:
+        max_value = int(self._total_thickness)
         forces: list[float] = []
 
-        for value in range(maxValue):
+        for value in range(max_value):
             forces.append(abs(self._force(diameter, float(value), modell)))
 
         return float(forces.index(min(forces)))
 
-    def _force(self, diameter: float, neutral_axis: float, modell: list[VGBendingMaterialData]) -> float:
-        maxValue = int(self._total_thickness)
+    def _force(self, diameter: float, neutral_axis: float,
+               modell: list[VGBendingMaterialData]) -> float:
+        max_value = int(self._total_thickness)
         force = 0.0
         width = 12e-3
 
-        for pos in range(maxValue):
+        for pos in range(max_value):
             epsilon = self._strain(float(pos), neutral_axis, diameter)
-            maxPos = 0.0
+            max_pos = 0.0
 
             for material in modell:
-                maxPos += material.thickness
-                if float(pos) < maxPos:
+                max_pos += material.thickness
+                if float(pos) < max_pos:
                     force += self._stress(epsilon, material) * 1e-6 * width
                     break
 
         return force
 
-    def _strain(self, pos: float, neutral_axis: float, diameter: float) -> float:
-        return (pos - neutral_axis) * 1e-6 / (diameter / 2 * 1e-3 + neutral_axis * 1e-6)
+    def _strain(self, pos: float, neutral_axis: float,
+                diameter: float) -> float:
+        return (pos - neutral_axis) * 1e-6 / (diameter / 2 * 1e-3 +
+                                              neutral_axis * 1e-6)
 
     def _stress(self, strain: float, material: VGBendingMaterialData) -> float:
         max_strain1 = material.sigma1 / material.E1
-        max_strain2 = max_strain1 + \
-            (material.sigma2 - material.sigma1) / material.E2
+        max_strain2 = max_strain1 + (material.sigma2 -
+                                     material.sigma1) / material.E2
 
         if strain >= 0:
             if strain > max_strain2:
@@ -149,7 +158,7 @@ class VGBendingSolver():
                 return -material.sigma1 + (strain + max_strain1) * material.E3
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     solver = VGBendingSolver()
 
     print("Loading Modell...")
@@ -163,11 +172,11 @@ if __name__ == '__main__':
     print("\nCalculating...")
     solver.solve()
 
-    print(
-        f"Minimum Bending Diameter Downward is: {solver.min_bending_diameter_d}")
+    print(f"Minimum Bending Diameter Downward is: "
+          f"{solver.min_bending_diameter_d}")
 
-    print(
-        f"Minimum Bending Diameter Upwards is: {solver.min_bending_diameter_u}")
+    print(f"Minimum Bending Diameter Upwards is: "
+          f"{solver.min_bending_diameter_u}")
 
-    print(
-        f"\nMinimum Double Bending Diameter is: {solver.min_double_bending_diameter}")
+    print(f"\nMinimum Double Bending Diameter is: "
+          f"{solver.min_double_bending_diameter}")
